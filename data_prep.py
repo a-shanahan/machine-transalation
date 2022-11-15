e5daa0751224b4b47dab53ecbf8b76e99c84a0ee
Alex <alex.m.shanahan@gmail.com>
2022-11-15 17:09:20 +0000

    Initial commit

---
+++ data_prep.py
@@ -0,0 +1,38 @@
+import logging
+import time
+
+import numpy as np
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import tensorflow_text
+
+
+# Download dataset
+examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
+                               with_info=True,
+                               as_supervised=True)
+
+train_examples, val_examples = examples['train'], examples['validation']
+
+
+for pt_examples, en_examples in train_examples.batch(3).take(1):
+  print('> Examples in Portuguese:')
+  for pt in pt_examples.numpy():
+    print(pt.decode('utf-8'))
+  print()
+
+  print('> Examples in English:')
+  for en in en_examples.numpy():
+    print(en.decode('utf-8'))
+
+model_name = 'ted_hrlr_translate_pt_en_converter'
+tf.keras.utils.get_file(
+    f'{model_name}.zip',
+    f'https://storage.googleapis.com/download.tensorflow.org/models/{model_name}.zip',
+    cache_dir='.', cache_subdir='', extract=True
+)
+
+tokenizers = tf.saved_model.load(model_name)
+
+[item for item in dir(tokenizers.en) if not item.startswith('_')]
